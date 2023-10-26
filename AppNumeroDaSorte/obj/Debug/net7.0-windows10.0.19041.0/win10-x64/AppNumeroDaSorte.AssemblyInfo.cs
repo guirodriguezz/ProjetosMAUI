d3bfa77e40089bf7ab98f31f1a5ef494ed915c0c
GUIPC\guiro <gui.rodrigues@msn.com>
2023-10-26 18:03:33 -0300

    SPlashscreen e icone

--- AppNumeroDaSorte/obj/Debug/net7.0-windows10.0.19041.0/win10-x64/AppNumeroDaSorte.AssemblyInfo.cs
+++ AppNumeroDaSorte/obj/Debug/net7.0-windows10.0.19041.0/win10-x64/AppNumeroDaSorte.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 
 [assembly: System.Reflection.AssemblyMetadataAttribute("Microsoft.Maui.ApplicationModel.AppInfo.PackageName", "cc2cb4ff-81c6-4ae1-853f-357d328b25ea")]
 [assembly: System.Reflection.AssemblyMetadataAttribute("Microsoft.Maui.ApplicationModel.AppInfo.PublisherName", "User Name")]
-[assembly: System.Reflection.AssemblyMetadataAttribute("Microsoft.Maui.ApplicationModel.AppInfo.Name", "AppNumeroDaSorte")]
+[assembly: System.Reflection.AssemblyMetadataAttribute("Microsoft.Maui.ApplicationModel.AppInfo.Name", "Número da Sorte")]
 [assembly: System.Reflection.AssemblyMetadataAttribute("Microsoft.Maui.ApplicationModel.AppInfo.Version", "1.0.0.1")]
 [assembly: System.Reflection.AssemblyCompanyAttribute("AppNumeroDaSorte")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
