c1ef650bba36506535cd4288583aae9016e705eb
GUIPC\guiro <gui.rodrigues@msn.com>
2023-10-26 15:04:34 -0300

    Alterando versao e mudando nome do projeto.

--- AppNumeroDaSorte/obj/Debug/net7.0-windows10.0.19041.0/win10-x64/AppNumeroDaSorte.AssemblyInfo.cs
+++ AppNumeroDaSorte/obj/Debug/net7.0-windows10.0.19041.0/win10-x64/AppNumeroDaSorte.AssemblyInfo.cs
@@ -18,7 +18,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("AppNumeroDaSorte")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
 [assembly: System.Reflection.AssemblyProductAttribute("AppNumeroDaSorte")]
 [assembly: System.Reflection.AssemblyTitleAttribute("AppNumeroDaSorte")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
