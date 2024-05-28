91c8dcbcf1ac9cc047e195e7eddd1cfd1881ddc9
Guilherme Alexandre Marques <gui.rodrigues@msn.com>
2024-05-28 00:27:26 -0300

    Adiciando métodos ao TransactionRepositorie.

--- AppControleFinanceiro/Models/Transaction.cs
+++ AppControleFinanceiro/Models/Transaction.cs
@@ -1,10 +1,11 @@
 using System;
+using LiteDB;
 
 namespace AppControleFinanceiro.Models;
 
 public class Transaction
 {
-    public int Id { get; set; }
+    public ObjectId? Id { get; set; }
     public TransactionType Type { get; set; }
     public string Name { get; set; }
     public DateTimeOffset Date { get; set; }
--- AppControleFinanceiro/Repositories/TransactionRepositorie.cs
+++ AppControleFinanceiro/Repositories/TransactionRepositorie.cs
@@ -1,32 +1,47 @@
 using System.Collections.Generic;
 using AppControleFinanceiro.Models;
+using LiteDB;
 
-namespace AppControleFinanceiro.Repositories;
+namespace AppControleFinanceiro.Repositories;   
 
 public class TransactionRepositorie : ITransactionRepositorie
 {
+    private readonly LiteDatabase _database;
+    private const string CollectionName = "transactions";
+
     public TransactionRepositorie()
     {
-        
+        _database = new LiteDatabase("Filename=C://users/AppData/database.db;Connection=Shared");
     }
 
     public List<Transaction> GetAll()
     {
-        throw new System.NotImplementedException();
+        return _database
+            .GetCollection<Transaction>(CollectionName)
+            .Query()
+            .OrderByDescending(a => a.Date)
+            .ToList();
     }
 
     public void Add(Transaction transaction)
     {
-        throw new System.NotImplementedException();
+        var col = _database.GetCollection<Transaction>(CollectionName);
+
+        col.Insert(transaction);
+        col.EnsureIndex(a => a.Date);
     }
 
     public void Update(Transaction transaction)
     {
-        throw new System.NotImplementedException();
+        var col = _database.GetCollection<Transaction>(CollectionName);
+
+        col.Update(transaction);
     }
 
     public void Delete(Transaction transaction)
     {
-        throw new System.NotImplementedException();
+        var col = _database.GetCollection<Transaction>(CollectionName);
+
+        col.Delete(transaction.Id);
     }
 }
